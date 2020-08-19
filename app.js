7764a85e07355e0c71d1dc7e654ae1a81fb8ef11
YASH SANJAY SALVI <46518495+Raxy45@users.noreply.github.com>
2020-08-20 00:04:28 +0530

    Bootstrap v1.04 12.00am

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-for (var i = 0; i < 7; i++) {
+for (var i = 0; i < 14; i++) {
   document
     .querySelectorAll("button")
     [i].addEventListener("click", function (event) {
-      console.log(this);
+      console.log(event.srcElement.classList[0]);
       playSound(event.srcElement.classList[0]);
       addAnimation(event.srcElement.classList[0]);
     });
 }
 document.addEventListener("keydown", function (event) {
-  playSound(event.key);
-  addAnimation(event.key);
+  var uppercase = event.key.toUpperCase();
+  playSound(uppercase);
+  addAnimation(uppercase);
 });
 function addAnimation(buttonClass) {
   document.querySelector("." + buttonClass).classList.add("pressed");
@@ -20,25 +21,25 @@ function addAnimation(buttonClass) {
 }
 
 function playSound(buttonClass) {
-  if (buttonClass === "w") {
+  if (buttonClass === "w" || buttonClass === "W") {
     var audio = new Audio("sounds/tom-1.mp3");
     audio.play();
-  } else if (buttonClass === "a") {
+  } else if (buttonClass === "a" || buttonClass === "A") {
     var audio = new Audio("sounds/tom-2.mp3");
     audio.play();
-  } else if (buttonClass === "s") {
+  } else if (buttonClass === "s" || buttonClass === "S") {
     var audio = new Audio("sounds/tom-3.mp3");
     audio.play();
-  } else if (buttonClass === "d") {
+  } else if (buttonClass === "d" || buttonClass === "D") {
     var audio = new Audio("sounds/tom-4.mp3");
     audio.play();
-  } else if (buttonClass === "j") {
+  } else if (buttonClass === "j" || buttonClass === "J") {
     var audio = new Audio("sounds/crash.mp3");
     audio.play();
-  } else if (buttonClass === "k") {
+  } else if (buttonClass === "k" || buttonClass === "K") {
     var audio = new Audio("sounds/kick-bass.mp3");
     audio.play();
-  } else if (buttonClass === "l") {
+  } else if (buttonClass === "l" || buttonClass === "L") {
     var audio = new Audio("sounds/snare.mp3");
     audio.play();
   }
